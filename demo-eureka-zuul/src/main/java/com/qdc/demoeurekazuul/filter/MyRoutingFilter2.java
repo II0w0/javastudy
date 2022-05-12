44c36c8e4ae9347c8fa32d4313baec6490dbdb1a
II0w0 <1441505767@qq.com>
2022-05-12 15:10:23 +0800

    demo-eureka-zuul V1.0 版本到本地仓库

---
+++ demo-eureka-zuul/src/main/java/com/qdc/demoeurekazuul/filter/MyRoutingFilter2.java
@@ -0,0 +1,23 @@
+package com.qdc.demoeurekazuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+
+public class MyRoutingFilter2 extends ZuulFilter {
+    @Override
+    public boolean shouldFilter(){return false;}
+
+    @Override
+    public Object run() throws ZuulException {
+        System.out.println("MyRoutingFilter2");
+        return null;
+    }
+
+    @Override
+    public String filterType(){return "route";}
+
+    @Override
+    public int filterOrder(){
+        return 0;
+    }
+}
